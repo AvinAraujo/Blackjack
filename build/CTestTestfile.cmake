03a5f9a7da96870c7e5c55bff750c744d8a3b760
Avin <AvinAraujo340@gmail.com>
2025-02-28 20:33:22 -0700

    I have no clue what im doing

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/14804/Desktop/Blackjack
+# Build directory: C:/Users/14804/Desktop/Blackjack/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
